957982a05e0d3afe8a0531b24782779011845f57
martinnebesar <nebesar.martin@gmail.com>
2020-02-28 18:45:28 -0500

    Chapter 9 uploaded

---
+++ Ch09Ex01/Ch09Ex01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch09Ex01
+{
+    public abstract class MyBase
+    { }
+
+    internal class MyClass : MyBase
+    { }
+
+    public interface IMyBaseInterface
+    { }
+
+    internal interface IMyBaseInterface2
+    { }
+
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2
+    { }
+
+    internal sealed class MyComplexClass : MyClass, IMyInterface
+    { }
+
+    class Praogram
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass();
+            Console.WriteLine(myObj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
